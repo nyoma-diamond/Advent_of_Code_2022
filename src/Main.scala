676fdbb4843ce666ff3c4b93b1ae9b29e15168a3
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2022-12-02 03:53:33 -0500

    Day 2

--- src/Main.scala
+++ src/Main.scala
@@ -5,6 +5,7 @@ object Main {
 
     /**
      * Day 1 part 1: Find the maximum total calories among the elves
+     *
      * @param path path to input file
      * @return maximum total calories
      */
@@ -24,7 +25,8 @@ object Main {
 
     /**
      * Day 1 part 2: Find the sum of the highest `n` total calories among the elves
-     * Note: Problem originally requested 3. Abstracted to `n` so this solution can also handle part 1!
+     * Note: Problem originally requested highest 3. Abstracted to highest `n` so this solution can also handle part 1!
+     *
      * @param path path to input file
      * @param n number of highest values to get
      * @return sum of highest `n` total calories
@@ -43,9 +45,52 @@ object Main {
     }
 
 
+    /**
+     * Day 2 part 1: Find the total score for the rock-paper-scissors competition
+     * A/X = Rock; B/Y = Paper; C/X = Scissors
+     *
+     * @param path path to input file
+     * @return total score for competition
+     */
+    def day2part1(path: String): Int = {
+        Using(Source.fromFile(path)) { data =>              // load input data file
+            data.getLines()                                 // for each line in file
+                .foldLeft(0)(                               // initialize partial score to 0
+                    (score: Int, cur: String) =>            // given partial score and current line
+                        score +                             // add to partial score
+                          cur(2) - 'X' + 1 +                // add points for hand choice
+                          ((cur(2) - cur(0) - 1) % 3) * 3   // add points for win/draw/loss (wacky math Just Works)
+                )
+        }.get                                               // get final result
+    }
+
+
+    /**
+     * Day 2 part 2: Find the total score for the rock-paper-scissors competition
+     * A = Rock; B = Paper; C = Scissors; X = Lose; Y = Draw; Z = Win
+     *
+     * @param path path to input file
+     * @return total score for competition
+     */
+    def day2part2(path: String): Int = {
+     Using(Source.fromFile(path)) { data =>                 // load input data file
+            data.getLines()                                 // for each line in file
+                .foldLeft(0)(                               // initialize partial score to 0
+                    (score: Int, cur: String) =>            // given partial score and current line
+                        score +                             // add to partial score
+                          (cur(2) - 'X') * 3 +              // add points for win/draw/loss
+                          (cur(2) + cur(0) - 1) % 3 + 1     // add points for hand choice (wacky math Just Works)
+                )
+        }.get                                               // get final result
+    }
+
+
     def main(args: Array[String]): Unit = {
         println("Day 1 part 1: " + day1Part1("./in/day1.txt"))
         println("Day 1 part 2: " + day1Part2("./in/day1.txt", 3))
         println("Day 1 part 1 (using part 2 implementation): " + day1Part2("./in/day1.txt", 1))
+
+        println("Day 2 part 1: " + day2part1("./in/day2.txt"))
+        println("Day 2 part 2: " + day2part2("./in/day2.txt"))
     }
 }
